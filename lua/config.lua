6d1573000783eab2c72e4e244a453ee5aed360ca
Rafael Lima <rafa.cbl@gmail.com>
2022-05-29 10:12:32 -0300

    set tabstop and shiftwidth based on file extension (ruby, go, yaml)

--- lua/config.lua
+++ lua/config.lua
@@ -12,3 +12,8 @@ vim.api.nvim_set_option("shell", "fish")
 -- colorscheme
 vim.cmd[[colorscheme NeoSolarized]]
 
+-- indentation based on file extension
+vim.cmd[[autocmd FileType go setlocal shiftwidth=4 tabstop=4]]
+vim.cmd[[autocmd FileType yaml setlocal shiftwidth=2 tabstop=2]]
+vim.cmd[[autocmd FileType ruby setlocal shiftwidth=2 tabstop=2]]
+
