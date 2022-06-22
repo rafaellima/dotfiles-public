47377537660375d0d5b2fd73be2726802f1d3a64
Rafael Lima <rafa.cbl@gmail.com>
2022-06-22 06:08:18 -0300

    fix: remove indent option from treesitter as it doesn't work well with ruby

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,17 +1,14 @@
-local status_ok, telescope = pcall(require, "nvim-treesitter")
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
   return
 end
 
-require'nvim-treesitter.configs'.setup {
+treesitter.setup {
   ensure_installed = "all", -- Only use maintained parsers
   ignore_install = { "phpdoc" }, -- required until issue https://github.com/claytonrcarter/tree-sitter-phpdoc/issues/15 is closed
 
   highlight = {
     enable = true,
-  },
-  indent = {
-    enable = true
   },
   endwise = {
     enable = true
