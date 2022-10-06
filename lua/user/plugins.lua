cb045d1b63beab0fd7dc537903a7f5c01ea743c2
Rafael Lima <rafa.cbl@gmail.com>
2022-10-06 14:11:59 -0300

    configuring nvim lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -13,35 +13,7 @@ require('packer').startup(function()
   }
 
   -- LSP
-  use {
-    "williamboman/nvim-lsp-installer",
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-	require("nvim-lsp-installer").setup({
-          automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-          opts = {
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { 'vim', 'use' }
-                }
-              }
-            }
-          },
-          ui = {
-            icons = {
-              server_installed = "✓",
-              server_pending = "➜",
-              server_uninstalled = "✗"
-            }
-          }
-        })
-	local lspconfig = require("lspconfig")
-	lspconfig.sumneko_lua.setup {}
-      end
-    }
-  }
+  use 'neovim/nvim-lspconfig'
 
   -- cmp plugins
   use 'hrsh7th/cmp-nvim-lsp'
